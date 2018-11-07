12a83acd8fd601f225870f5215a94e88b69d4a42
arghasen10 <arghasen10@gmail.com>
2018-11-08 00:00:55 +0530

    First commit

---
+++ app/src/main/java/com/logui/arghasen/firebaseapp/MainActivity.java
@@ -0,0 +1,64 @@
+package com.logui.arghasen.firebaseapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> myArrayList = new ArrayList<>();
+    ListView myListView;
+    Firebase myFirebase;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Firebase.setAndroidContext(this);
+        myFirebase = new Firebase("https://fir-app-b50e6.firebaseio.com/");
+
+        final ArrayAdapter<String> myArrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,myArrayList);
+        ListView myListView = (ListView) findViewById(R.id.ListView);
+        myListView.setAdapter(myArrayAdapter);
+
+        myFirebase.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                String myChildValues = dataSnapshot.getValue(String.class);
+                myArrayList.add(myChildValues);
+                myArrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                myArrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+    }
+}
